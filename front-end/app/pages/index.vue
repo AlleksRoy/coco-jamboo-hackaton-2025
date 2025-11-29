c276f4dbcf615d3672aed48ed999047bfb0ed6dc
Arsenii.O <arsenii.opryshko@gmail.com>
2025-11-29 17:19:57 +0200

    initial commit front-end

---
+++ front-end/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-dark-black">hello</h1>
+  </div>
+</template>
---
+++ front-end/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@nuxt/ui',
+    '@pinia/nuxt',
+  ],
+
+  srcDir: 'app/',
+  css: [
+    '~/assets/styles/index.css',
+  ],
+
+})
